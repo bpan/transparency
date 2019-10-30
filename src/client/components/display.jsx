97e0afd45b1ad966fe7f2fe136203c569b0c19ef
Brian Pan <bwpan@yahoo.com>
2019-10-29 21:43:23 -0700

    Added next song fade transition

--- src/client/components/display.jsx
+++ src/client/components/display.jsx
@@ -1,5 +1,6 @@
-import Measure from 'react-measure'
+import Measure from 'react-measure';
 import React from 'react';
+import { CSSTransition } from 'react-transition-group';
 
 import './display.scss';
 import MonitorVerse from "./monitorVerse.jsx";
@@ -9,38 +10,42 @@ class Display extends React.Component {
   constructor(props) {
     super(props);
     this.state = this.getStorageState();
+    this.state.fadeOutForNextSong = false;
     this.state.verseHeights = new Array(this.state.setlist.length);
 
-    this.storageHandler = this.storageHandler.bind(this);
+    this.storageChangeHandler = this.storageChangeHandler.bind(this);
   }
 
   render() {
     return (
       <div>
         <div id='content'>
-          <div className="song" style={{top: -this.state.verseHeights.slice(0, this.state.currentVerse).reduce((a, b) => a + b, -20) + 'px'}}>
-            {this.state.currentSong !== null && this.state.setlist && this.state.setlist[this.state.currentSong]
-              ?
-              <div>
-                <div className="title">{this.state.setlist[this.state.currentSong].title}</div>
-                {this.state.setlist[this.state.currentSong].verses.map((verse, index) =>
-                  <Measure bounds onResize={contentRect => {
-                    const verseHeights = [...this.state.verseHeights];
-                    verseHeights[index] = contentRect.bounds.height;
-                    this.setState({verseHeights});
-                  }}>
-                    {({ measureRef }) => (
-                      <div ref={measureRef}>
-                        <MonitorVerse currentVerse={index === this.state.currentVerse} verseNumber={index + 1} verseText={verse}/>
-                      </div>
-                    )}
-                  </Measure>
-                )}
-              </div>
-              :
-              null
-            }
-          </div>
+          <CSSTransition classNames="song" in={this.state.currentSong !== null && !this.state.fadeOutForNextSong}
+                         timeout={200} onExited={() => this.fadeInNextSong()}>
+            <div className="song" style={{top: -this.state.verseHeights.slice(0, this.state.currentVerse).reduce((a, b) => a + b, -20) + 'px'}}>
+              {this.state.currentSong !== null && this.state.setlist && this.state.setlist[this.state.currentSong]
+                ?
+                <div>
+                  <div className="title">{this.state.setlist[this.state.currentSong].title}</div>
+                  {this.state.setlist[this.state.currentSong].verses.map((verse, index) =>
+                    <Measure bounds onResize={contentRect => {
+                      const verseHeights = [...this.state.verseHeights];
+                      verseHeights[index] = contentRect.bounds.height;
+                      this.setState({verseHeights});
+                    }}>
+                      {({measureRef}) => (
+                        <div ref={measureRef}>
+                          <MonitorVerse currentVerse={index === this.state.currentVerse} verseNumber={index + 1} verseText={verse}/>
+                        </div>
+                      )}
+                    </Measure>
+                  )}
+                </div>
+                :
+                null
+              }
+            </div>
+          </CSSTransition>
         </div>
 
         <div id='black-screen' className={this.state.screenIsBlack ? 'screen-is-black' : ''}/>
@@ -61,16 +66,30 @@ class Display extends React.Component {
     };
   }
 
-  storageHandler(e) {
-    this.setState(this.getStorageState());
+  storageChangeHandler(e) {
+    const storageState = this.getStorageState();
+    const fadeOutForNextSong = this.state.currentSong !== null && this.state.currentSong !== storageState.currentSong;
+    if (fadeOutForNextSong) {
+      // Preserve current state and fade out
+      this.setState({fadeOutForNextSong});
+    } else {
+      // Update current state
+      this.setState({...storageState, fadeOutForNextSong})
+    }
+  }
+
+  fadeInNextSong() {
+    const storageState = this.getStorageState();
+    const fadeOutForNextSong = false;
+    this.setState({...storageState, fadeOutForNextSong})
   }
 
   componentDidMount() {
-    window.addEventListener('storage', this.storageHandler);
+    window.addEventListener('storage', this.storageChangeHandler);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('storage', this.storageHandler);
+    window.removeEventListener('storage', this.storageChangeHandler);
   }
 }
 
