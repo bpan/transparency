dc03a8ad107f2bb44b039e15a11a2f7194aae1ba
Brian Pan <bwpan@yahoo.com>
2019-12-13 22:10:17 -0800

    Refactored the controller page to render via a router

--- src/client/controller.js
+++ src/client/controller.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import {render} from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 import Controller from './components/controller.jsx';
 
-render(<Controller/>, document.getElementById('app'));
+render(
+  <Router>
+    <div>
+      <Route exact path='/' component={Controller}/>
+    </div>
+  </Router>,
+  document.getElementById('app')
+);
